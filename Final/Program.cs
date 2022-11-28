3d82dab0cf6639d7f5864ffb3c70a568bd96f558
Dmitry Nagovitsyn <nagovicinde@mail.ru>
2022-11-28 21:38:37 +0400

    added function for Creating array

--- Final/Program.cs
+++ Final/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3.
+
+int Prompt(string message)                                 //Ввод числа с клавиатуры
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+string[] CreateArray(int length)                            // Создание массива
+{
+    string[] massiv = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        System.Console.WriteLine($"Введите {i+1}й элемент: ");
+        string line = Convert.ToString(Console.ReadLine());
+        massiv[i] = line;
+    }
+    return massiv;
+}
+
